8b01212d98ed3b94c12bf65adf5798c2b45904f6
Jose Maria Lejeune <jose.lejeune@educa.madrid.org>
2022-03-10 10:46:05 +0100

    Prueba de actualización del código

--- otroproyecto/src/main/java/principal/Lanzador.java
+++ otroproyecto/src/main/java/principal/Lanzador.java
@@ -4,6 +4,15 @@ public class Lanzador {
 
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
+		/**
+		 * Clase prueba de la tutoria
+		 */
+		 public static main () {
+			 /**
+			  * empezamos a escribir la clase main
+			  */
+			 
+		 }
 
 	}
 
