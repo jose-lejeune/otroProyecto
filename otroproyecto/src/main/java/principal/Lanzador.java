abbd1820ac68039088b63c9d314d10ef90fbe3d4
Jose Maria Lejeune <jose.lejeune@educa.madrid.org>
2022-03-10 18:08:07 +0100

    Creamos el constructor Lanzador(String orden)

--- otroproyecto/src/main/java/principal/Lanzador.java
+++ otroproyecto/src/main/java/principal/Lanzador.java
@@ -5,10 +5,22 @@ public class Lanzador {
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
 		/**
-		 * Clase prueba de la tutoria
+		 * Clase prueba de la tutoria -- modificamos el codigo
 		 */
-		 
+		String orden = "sumar";
+		Lanzador mio = new Lanzador(orden);
 
+	}
+
+	public void Lanzador(String orden) {
+		switch (orden) {
+		case "sumar":
+
+			break;
+		default:
+
+			break;
+		}
 	}
 
 }
